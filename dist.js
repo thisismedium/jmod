e8b7872a98beefcb436363d6d68f58322e0ce115
James Long <james@coptix.com>
2008-12-02 15:53:10 -0500

    js: bootstraping tweaks

--- dist.js
+++ dist.js
@@ -1,13 +1,19 @@
 
-function load_dist(version) {
-    function file(name) {
-	return "http://js.coptix.com/" + version + "/" + name + ".js";
-    }
+var Distribution = {
+    load: function(version, base) {
+	base = base || 'http://js.coptix.com/';
+	if(base.charAt(base.length-1) != '/')
+	    base += '/';
+	
+	function file(name) {
+	    return base + version + "/" + name + ".js";
+	}
 
-    window.__dist_version__ = version
-   
-    document.write('<script type="text/javascript" ' +
-		   'src="' + file('modules') + '"></script>');
-    document.write('<script type="text/javascript" ' +
-		   'src="' + file('dist') + '"></script>');
-}
+	window.__dist_version__ = version;
+	window.__dist_url__ = base;
+	
+	document.write('<script type="text/javascript" ' +
+		       'src="' + file('modules') + '"></script>');
+	document.write('<script type="text/javascript" ' +
+		       'src="' + file('dist') + '"></script>');
+    }};
