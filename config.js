5c1c65d243b784e64ae0b1451c3fd8a9b6373e60
James Long <james@coptix.com>
2008-12-09 19:28:16 -0500

    js: removing config & dist tweaks

--- config.js
+++
@@ -1,4 +0,0 @@
-
-// These variables are loaded in when a site loads
-// modules.js
-window.__dist_url__ = "http://js.coptix.com";
--- dist.js
+++ dist.js
@@ -1,21 +1,36 @@
 //
-// Installs the centralized distribution
-// @version 0.0.1
+// Installs the `lib' namespace which pulls
+// the module from the central repository.
 //
-
+// This file is NOT copied per distribution,
+// so any changes affect ALL sites.  This is done
+// purposefully so that we can change any internal
+// structure later on, but change this very
+// carefully.
+//
 (function() {
 
     function DistributionException(msg) {
 	this.msg = msg;
-    }
-
-    DistributionException.prototype.toString = function() {
-	return this.msg;
+	this.toString = function() {
+	    return this.msg;
+	}
     }
     
-    // Create the LIB namespace which dispatches requests for
+    // Create the `lib' namespace which dispatches requests for
     // modules off to the centralized repository
     Modules.set_namespace_handler('lib', function(names) {
+	if(!window.__dist_url__) {
+	    throw new DistributionException("No distribution url found, " +
+					    "check the distribution.");
+	}
+
+	if(!window.__dist_version__) {
+	    throw new DistributionException("No distribution version " +
+					    "found, check the " +
+					    "distribution");
+	}
+	
 	dist_url = window.__dist_url__.replace(/\/*$/, "");
 	for(var i=0; i<names.length; i++) {
 	    var file = names[i].substr(names[i].indexOf('.')+1) + '.js';
