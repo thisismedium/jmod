757555b7ec9eaf897c8d97e91453708f163e4446
James Long <james@coptix.com>
2008-12-09 19:15:24 -0500

    js: bug fixes, resources, tweaking loading process

---
+++ config.js
@@ -0,0 +1,4 @@
+
+// These variables are loaded in when a site loads
+// modules.js
+window.__dist_url__ = "http://js.coptix.com";
--- dist.js
+++ dist.js
@@ -1,60 +1,28 @@
+//
+// Installs the centralized distribution
+// @version 0.0.1
+//
 
 (function() {
 
-    function ResourceException(name, msg) {
+    function DistributionException(msg) {
 	this.msg = msg;
-	this.toString = function() {
-	    return this.name + ": " + this.msg;
-	}
     }
 
-    window.Distribution = {
-	load: function(version, base) {
-	    base = base || 'http://js.coptix.com/';
-	    if(base.charAt(base.length-1) != '/')
-		base += '/';
-	    
-	    function file(name) {
-		return base + version + "/" + name + ".js";
-	    }
-
-	    window.__dist_version__ = version;
-	    window.__dist_url__ = base;
-	    
-	    document.write('<script type="text/javascript" ' +
-			   'src="' + file('modules') + '"></script>');
-	    document.write('<script type="text/javascript" ' +
-			   'src="' + file('dist') + '"></script>');
-	},
-
-	get_resource: function(name) {
-	    if(!__dist_url__ || !__dist_version__) {
-		throw ResourceException(name,
-					"No information about the" +
-					"distribution available! " +
-					"(have you called " +
-					"Distribution.load(...)?)");
-	    }
-	    
-	    var module = arguments.callee.caller.module;
-	    return __dist_url__ + __dist_version__ + "/" +
-		module.name + "/" + name;
-	},
-
-	load_resource: function(name) {
-	    url = Distribution.get_resource(name);
-	    ext = url.substring(url.lastIndexOf('.')+1);
-	    
-	    switch(ext) {
-	    case 'css':
-		document.write('<link rel="stylesheet" href="' + url +
-			       '" type="text/css" />');
-		break;
-	    default:
-		throw ResourceException(name, "Method unknown for " +
-					"loading this resource type");
-	    }
+    DistributionException.prototype.toString = function() {
+	return this.msg;
+    }
+    
+    // Create the LIB namespace which dispatches requests for
+    // modules off to the centralized repository
+    Modules.set_namespace_handler('lib', function(names) {
+	dist_url = window.__dist_url__.replace(/\/*$/, "");
+	for(var i=0; i<names.length; i++) {
+	    var file = names[i].substr(names[i].indexOf('.')+1) + '.js';
+	    Modules.request(names[i],
+			    dist_url + "/" + window.__dist_version__,
+			    file);
 	}
-    };
+    });
 
 })();
